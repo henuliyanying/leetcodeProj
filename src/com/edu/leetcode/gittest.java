a2f9dae75bbb354fe347b84180d81df6dad6d628
henuliyanying <henuliyanying@aliyun.com>
2021-09-28 17:52:55 +0800

    更新代码并上传

--- src/com/edu/leetcode/gittest.java
+++ src/com/edu/leetcode/gittest.java
@@ -13,5 +13,6 @@ public class gittest {
         System.out.println("iml文件是IDEA自动创建的模块文件，用于Java开发，存储一些模块开发相关信息，比如一个java组件" +
                 "");
         System.out.println("Git好难学习呀");
+        System.out.println("VCS中更改远程仓库地址");
     }
 }
